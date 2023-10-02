c0081455fc6c09427d9a4804a09bb80aca52cc77
Roquv13 <czarekolszewski99@gmail.com>
2023-10-02 19:58:06 +0200

    Add scanner for collecting data from the user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        userInfo userInfo = new userInfo();
+        User user = new User();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("What's your surname?");
+        String surname = scanner.nextLine();
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        System.out.println("Give your email address:");
+        String email = scanner.nextLine();
+
+        user.basicInfo(name, surname, age, email);
     }
 }
