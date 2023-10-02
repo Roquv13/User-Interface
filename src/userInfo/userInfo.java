78fc133fe40b4e13acc98f18e68be640f1744a1e
Roquv13 <czarekolszewski99@gmail.com>
2023-10-02 19:17:37 +0200

    Add inital variables

---
+++ src/userInfo/userInfo.java
@@ -0,0 +1,16 @@
+package userInfo;
+
+public class userInfo {
+    //User basic infomations
+    String name;
+    String surname;
+    int age;
+    String email;
+
+    // User address
+    String street;
+    int buldingNumber;
+    int apartmentNumber;
+    String zipCode;
+    String city;
+}
