9fbaf5184aba690b8e9cc7556495e0461ac532a9
Cezary Olszewski <czarekolszewski99@gmail.com>
2023-10-03 10:06:28 +0200

    Add new check password

--- src/Account.java
+++ src/Account.java
@@ -24,19 +24,34 @@ public class Account {
 
     public void passwordCheck(String password) {
         this.password = password;
-        incorrectPassword = "Password is incorrect";
 
-        if (password.length() >= 8) {
+//        incorrectPassword = "Password is incorrect";
+//
+//        if (password.length() >= 8) {
+//            for (int i = 0; i < password.length(); i++) {
+//                char[] passwordChar = password.toCharArray();
+//                if (Character.isUpperCase(passwordChar[i])) uppercaseCount++;
+//                if (Character.isDigit(passwordChar[i])) digitCount++;
+//            }
+//            if (uppercaseCount >= 1 && digitCount >= 1) {
+//                System.out.println(password);
+//            } else System.out.println(incorrectPassword);
+//        }
+//        else System.out.println(incorrectPassword);
+
+        boolean passwordStatus = true;
+        if (passwordStatus) {
+            if (password.length() < 8) passwordStatus = false;
+
             for (int i = 0; i < password.length(); i++) {
                 char[] passwordChar = password.toCharArray();
-                if (Character.isUpperCase(passwordChar[i])) uppercaseCount++;
-                if (Character.isDigit(passwordChar[i])) digitCount++;
+                if (!Character.isUpperCase(passwordChar[i])) passwordStatus = false;
+                else passwordStatus = true;
+                if (!Character.isDigit(passwordChar[i])) passwordStatus = false;
+                else passwordStatus = true;
             }
-            if (uppercaseCount >= 1 && digitCount >= 1) {
-                String correctPassword = password;
-                System.out.println(password);
-            } else System.out.println(incorrectPassword);
-        }
-        else System.out.println(incorrectPassword);
+
+            if (passwordStatus) System.out.println(password);
+        } else System.out.println("Password is incorrect");
     }
 }
